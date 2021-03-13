7124e3f4465ba6ee548746b10c562b87df703779
Luca <lukamusso@gmail.com>
2021-03-13 16:13:57 +0100

    Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=lukamu/ml-microservice
+imagename="ml-microservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservice --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+podname=$(kubectl get pods --no-headers -o custom-columns=":metadata.name")
+kubectl port-forward $podname 8000:80
+kubectl logs $podname
